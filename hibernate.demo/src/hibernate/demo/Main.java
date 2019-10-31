9f05ac3aea071f3ff44858ee139ef41adcb1252b
hasan.turan <hasan.turan@KM-0545-119.kgk.local>
2019-10-31 17:03:20 +0300

    select and insert added

--- hibernate.demo/src/hibernate/demo/Main.java
+++ hibernate.demo/src/hibernate/demo/Main.java
@@ -9,15 +9,56 @@ import org.hibernate.cfg.Configuration;
 public class Main {
 
 	public static void main(String[] args) {
+		add();
+	}
+
+	public static void add() {
 		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(City.class)
 				.buildSessionFactory();
 
 		Session session = sessionFactory.getCurrentSession();
 		try {
 			session.beginTransaction();
-			List<City> cities = session.createQuery("from City").getResultList(); // City:name of entity not table
-			for (City city : cities) {
-				System.out.println(city.getName());
+			// HQL --> Hibernate query language
+
+			City city = new City();
+			city.setName("Düzce Hibernate");
+			city.setCountryCode("TUR");
+			city.setDistrict("Düzce");
+			city.setPopulation(75000);
+			session.save(city);
+
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			sessionFactory.close();
+		}
+	}
+
+	public static void select() {
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(City.class)
+				.buildSessionFactory();
+
+		Session session = sessionFactory.getCurrentSession();
+		try {
+			session.beginTransaction();
+			// HQL --> Hibernate query language
+			String query = "";
+			// query= "from City c where c.countryCode='TUR' and c.district='Ankara'"
+			// query = "from City c where c.name like '%kar%'";
+			// query = "from City c order by c.name desc";
+			// List<City> cities = session.createQuery(query).getResultList(); // City:name
+			// of entity not table
+//			for (City city : cities) {
+//				System.out.println(city.getName());
+//			}
+//			
+			query = "select c.countryCode from City c group by c.countryCode";
+			List<String> countryCodes = session.createQuery(query).getResultList(); // City:name of entity not table
+
+			for (String countryCode : countryCodes) {
+				System.out.println(countryCode);
 			}
 			session.getTransaction().commit();
 		} catch (Exception e) {
